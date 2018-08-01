ef00ca7298ce149b5d872c96ba12ef917312669b
Bridget Almas <balmsa@gmail.com>
2018-08-01 13:28:19 -0400

    don't set obligatory matches on treebank adapter created inflections

--- src/alpheiostb/adapter.js
+++ src/alpheiostb/adapter.js
@@ -106,8 +106,6 @@ class AlpheiosTreebankAdapter extends BaseAdapter {
       if (infl[localName]) {
         let obj = model.typeFeature(Models.Feature.types[featureType]).createFeatures(infl[localName][0]._text, 1)
         inflection.addFeature(obj)
-        // add this feature to this list of features for obligatory matching
-        inflection.constraints.obligatoryMatches.push(Models.Feature.types[featureType])
         if (addToLemma) {
           lemma.addFeature(obj)
         }
